3090ca0a4ef16dc75d768c2910bd60c1fdc5efa2
Rickard Korkko <rickardkorkko@gmail.com>
2017-09-26 18:19:39 +0200

    New field seperator

--- ankigen.py
+++ ankigen.py
@@ -3,4 +3,4 @@ import sys, json
 f = open(sys.argv[1],'r')
 input=json.load(f)
 for card in range(len(input)):
-        print('"' + input[card]['front'] + '"' + ';' + '"' + input[card]['back'] + '"' +';')
+        print('"' + input[card]['front'] + '"' +'\t' + '"' + input[card]['back'] + '"')
